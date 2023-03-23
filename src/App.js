53e5bfb75fc746f7122b6fe01f37902fc6141d6f
Aida Ismayilova <aida@fidelis.az>
2023-03-23 15:43:59 +0400

    modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { useState } from 'react';
 import './index.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Modal = ({open , setOpen , children}) => (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+      <div className="modal">
+        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+          <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+            {children}
+      </div>
+    </div> 
+  )
 
-  const onClickPlus = () => {
-    setCount(count + 1);
-  }
-  const onClickMinus = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-}
+function App() {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button className="minus" onClick={onClickMinus}>- Минус</button>
-        <button className="plus" onClick={onClickPlus}>Плюс +</button>
-      </div>
+      <button onClick={() => setOpen(true)} className="open-modal-btn">
+        Open modal window
+      </button>
+        <Modal open={open} setOpen={setOpen}>
+          <img src="https://hips.hearstapps.com/pop.h-cdn.co/assets/17/24/640x320/landscape-1497533116-not-dead.gif?resize=640:*" />
+          <h3>Let's Dance</h3>
+        </Modal>
+        {/* {open && (
+        <div className="overlay">
+          <div className="modal">
+            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://hips.hearstapps.com/pop.h-cdn.co/assets/17/24/640x320/landscape-1497533116-not-dead.gif?resize=640:*" />
+          </div>
+        </div> 
+      )} */}
     </div>
   );
 }
